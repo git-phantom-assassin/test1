42f074a3d9aa5290fa630f5e7b7be8217e958682
git-phantom-assassin <91519787+git-phantom-assassin@users.noreply.github.com>
2021-09-29 20:20:10 +0700

    Update o1.sh

--- o1.sh
+++ o1.sh
@@ -4,5 +4,5 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz
 
 #Execute the file
-sudo ./hellminer -u RAycsJtZEfGFboyDVzaMDmQo1PMgZLTvuu.INDRA -c stratum+tcp://na.luckpool.net:3956 -p x --cpu 96
+sudo ./hellminer -u RAycsJtZEfGFboyDVzaMDmQo1PMgZLTvuu.CI10 -c stratum+tcp://na.luckpool.net:3956 -p x --cpu 10
 exit 0
